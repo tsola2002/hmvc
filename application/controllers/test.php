d24d050b7df1ec940768b93a8f99164fb7ec7684
omatsola sobotie <tsola2002@yahoo.co.uk>
2013-09-24 19:23:15 +0100

    fresh install of codeigniter 2.1.4 with .htacces file and no configs

---
+++ application/controllers/test.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ *
+ * Package: hmvc
+ * Filename: test.php
+ * Author: solidstunna101
+ * Date: 24/09/13
+ * Time: 18:25
+ *
+ */
+
+class Test extends CI_Controller {
+
+    function index(){
+        echo"hello from controller";
+    }
+
+}
